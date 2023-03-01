9ca1c021a79267a99a990cb74c02775492a92108
wchen2296 <53542192+wchen2296@users.noreply.github.com>
2023-03-01 18:42:57 -0500

    cleared everything ready to make new branch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World</h1>
-      <MyButton/>
+      <h1>Sup</h1>
     </div>
   );
 }
